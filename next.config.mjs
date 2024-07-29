edfd2f01084d4856d33a44849e16a09b4fbdb43b
tlatawiec <latawiecthomas@gmail.com>
2024-07-29 14:15:34 -0500

    Redirects for next.js deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/api/:path*', // Match any path that starts with /api/
+        destination: 'https://api.poliapi.com/api/:path*', // Redirect to the api subdomain
+        permanent: true, // This will use a 308 status code for the redirect
+      },
+    ];
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
